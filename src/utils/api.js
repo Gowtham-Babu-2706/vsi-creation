b0e4d292e40a1c8dce7cc323dcf58ed090294687
Gowtham-Babu-2706 <gowthambabu27062006@gmail.com>
2026-06-26 12:22:50 +0530

    After adding the backend url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://vsibackend.onrender.com/api';
+const API_BASE_URL = 'https://vsibackend-4zlw.onrender.com/api';
 
 const getHeaders = (secured = false) => {
   const headers = {
